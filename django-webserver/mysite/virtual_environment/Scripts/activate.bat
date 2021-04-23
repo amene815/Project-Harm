be5633c71c04adf826191417064e6773f7f85b03
Austin Engle <42854353+amene815@users.noreply.github.com>
2021-04-23 10:44:15 -0400

    Updating the venv to correctly load the necessary packages

--- django-webserver/mysite/virtual_environment/Scripts/activate.bat
+++ django-webserver/mysite/virtual_environment/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=D:\College Stuff\Senior Year\django-webserver\mysite\virtual_environment"
+set "VIRTUAL_ENV=%~dp0..\"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
