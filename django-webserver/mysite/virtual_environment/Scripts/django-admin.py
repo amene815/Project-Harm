fa2876196ea389ce4443d29bc21b874ea93f22eb
Austin Engle <42854353+amene815@users.noreply.github.com>
2021-04-23 10:08:55 -0400

    Initial commit of the Django webserver

---
+++ django-webserver/mysite/virtual_environment/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\college stuff\senior year\django-webserver\mysite\virtual_environment\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
